03c11a355a9304c33b5e79a7f4c41184e48eab6a
Pratiksha <pratikshakolase13@gmail.com>
2024-10-26 11:56:04 +0530

    Modified the addition method in StringCalculator to handle the empty string inpu

--- Calculator/src/main/java/StringCalculator.java
+++ Calculator/src/main/java/StringCalculator.java
@@ -4,7 +4,9 @@ public class StringCalculator {
 
 
     public static int addition(String numbersToAdd){
-
+            if(numbersToAdd.isEmpty()){
+                return 0;
+            }
            String[] numbersArray = numbersToAdd.split(",");
           return Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
     }
