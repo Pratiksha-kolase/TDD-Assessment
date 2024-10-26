52d2f9c173634c53bdf01be6cb0e30bd255ffd15
Pratiksha <pratikshakolase13@gmail.com>
2024-10-26 12:27:15 +0530

    Added a test case for negative number in the input status - FAILED

--- Calculator/src/main/java/StringCalculator.java
+++ Calculator/src/main/java/StringCalculator.java
@@ -7,17 +7,16 @@ public class StringCalculator {
         if (numbersToAdd.isEmpty()) {
             return 0;
         }
-       String[] numbersArray = removeNewLine(numbersToAdd);
-//
+        String[] numbersArray = removeNewLine(numbersToAdd);
         return Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
     }
 
     private static String[] removeNewLine(String numberToAdd) {
-        if(numberToAdd.startsWith("//")) {
-            numberToAdd = numberToAdd.replace("//","\n");
+        if (numberToAdd.startsWith("//")) {
+            numberToAdd = numberToAdd.replace("//", "\n");
         }
-        if(numberToAdd.startsWith("//")) {
-            numberToAdd = numberToAdd.replace("//","\n");
+        if (numberToAdd.startsWith("//")) {
+            numberToAdd = numberToAdd.replace("//", "\n");
         }
         numberToAdd = numberToAdd.replaceAll("[,\\n;`^|#@$%&*()!\\t]+", ",").replaceAll("^,|,$", "");
         String[] separateNum = numberToAdd.split("[,\\n]");
--- Calculator/src/test/java/StringCalculatorTest.java
+++ Calculator/src/test/java/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
 
@@ -20,6 +21,14 @@ public class StringCalculatorTest {
     @Test
     public void testWithDifferentDelimiters(){
         assertEquals(3,StringCalculator.addition("//;\n1;2"));
+    }
+
+    @Test
+    public void testWithNegativeNumbers(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.addition("-1,2,3,-4");
+        });
+        assertEquals("negative numbers not allowed -1,-4", exception.getMessage());
     }
 
 }
