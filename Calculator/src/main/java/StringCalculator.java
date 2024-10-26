6e2f700c35ff32fd929fe3c0d8afd67c155fae68
Pratiksha <pratikshakolase13@gmail.com>
2024-10-26 12:05:57 +0530

    Modified the adition method to handle the '\n' in between the number and return the appropriate result

--- Calculator/src/main/java/StringCalculator.java
+++ Calculator/src/main/java/StringCalculator.java
@@ -3,11 +3,17 @@ import java.util.Arrays;
 public class StringCalculator {
 
 
-    public static int addition(String numbersToAdd){
-            if(numbersToAdd.isEmpty()){
-                return 0;
-            }
-           String[] numbersArray = numbersToAdd.split(",");
-          return Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
+    public static int addition(String numbersToAdd) {
+        if (numbersToAdd.isEmpty()) {
+            return 0;
+        }
+        numbersToAdd = removeNewLine(numbersToAdd);
+        String[] numbersArray = numbersToAdd.split(",");
+        return Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
+    }
+
+    private static String removeNewLine(String numberToAdd) {
+        numberToAdd = numberToAdd.replace("\n", ",");
+        return numberToAdd;
     }
 }
