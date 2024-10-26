f8aa8355aac694354715ff03e18fceb7b60bae30
Pratiksha <pratikshakolase13@gmail.com>
2024-10-26 11:54:45 +0530

    Implemented a method to perfom addition in StringCalculator - addition(String numbersToAdd), Along with created a test case to test the addition method with status - FAILED'

--- Calculator/src/main/java/StringCalculator.java
+++ Calculator/src/main/java/StringCalculator.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class StringCalculator {
+
+
+    public static int addition(String numbersToAdd){
+
+           String[] numbersArray = numbersToAdd.split(",");
+          return Arrays.stream(numbersArray).mapToInt(Integer::parseInt).sum();
+    }
 }
--- Calculator/src/test/java/StringCalculatorTest.java
+++ Calculator/src/test/java/StringCalculatorTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+
+    @Test
+    public void testWithEmptyString(){
+        assertEquals(0,StringCalculator.addition(""));
+    }
 }
